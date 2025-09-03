6d106c22632ea63bd779af788d9d59663eb8ac12
yotorng <yotorng@gmail.com>
2025-09-03 22:59:58 +0800

    Quartz sync: Sep 3, 2025, 10:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Instrument Serif",
+        header: "Petrona",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
